46f54b9cc2c500853ed4ba0d1934402430b2249c
D069516 <D069516@IGBN31649735A.emea.global.corp.sap>
2017-05-19 10:29:41 +0200

    implemented project structure (JPA, Spring, Maven)

---
+++ simpleBackEnd/src/main/java/model/Person.java
@@ -0,0 +1,50 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+	
+	@Id
+	@Column(name = "CUSTOMER_ID", length = 10)
+	private String customerId;
+	
+	@Column(name = "FIRST_NAME", length = 40)
+	private String firstName;
+
+	@Column(name = "LAST_NAME", length = 40)
+	private String lastName;
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+	
+	
+}
