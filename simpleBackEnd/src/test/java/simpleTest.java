f02dea7e50928101ea0051e854c6f13f5e15c84a
D069516 <D069516@IGBN31649735A.emea.global.corp.sap>
2017-05-26 09:12:30 +0200

    unit test working

---
+++ simpleBackEnd/src/test/java/simpleTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class simpleTest {
+
+	@Test
+	public void testGetCustomerId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCustomerId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLastName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLastName() {
+		fail("Not yet implemented");
+	}
+
+}
